2e14f460ce528b4cf0ae550c331d42932364289e
owolana ayomide <ayomideowolana1@gmail.com>
2022-07-11 22:58:35 +0100

    edited port

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/",router)
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
   log("Listening...");
 });
 
